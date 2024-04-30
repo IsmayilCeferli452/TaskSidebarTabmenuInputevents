d36fa5b4eb7fe563bb99c6b25316f4b118785eb9
Ismayil Ceferli <ismayilej@code.edu.az>
2024-04-30 23:09:03 +0400

    Finish

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,11 @@
 
 let images = document.querySelectorAll(".slider-container .container img");
 
-let icons = document.querySelectorAll(".slider-container .container .icons i");
+let iconRight = document.querySelectorAll(".slider-container .container .icons .right-icon");
 
-icons.forEach(item => {
+let iconLeft = document.querySelectorAll(".slider-container .container .icons .left-icon");
+
+iconRight.forEach(item => {
     item.addEventListener("click", function(){
         if(!images[0].classList.contains("d-none")){
             images[0].classList.add("d-none");
@@ -23,6 +25,27 @@ icons.forEach(item => {
             images[2].style.display = "none";
             images[0].classList.remove("d-none");
             images[0].style.display = "block"; 
+        }
+    })
+});
+
+iconLeft.forEach(item => {
+    item.addEventListener("click", function(){
+        if(!images[0].classList.contains("d-none")){
+            images[0].classList.add("d-none");
+            images[0].style.display = "none";           
+            images[2].classList.remove("d-none");
+            images[2].style.display = "block"; 
+        }else if(!images[1].classList.contains("d-none")){
+            images[1].classList.add("d-none");
+            images[1].style.display = "none";
+            images[0].classList.remove("d-none");
+            images[0].style.display = "block"; 
+        }else if(!images[2].classList.contains("d-none")){
+            images[2].classList.add("d-none");
+            images[2].style.display = "none";
+            images[1].classList.remove("d-none");
+            images[1].style.display = "block"; 
         }
     })
 });
